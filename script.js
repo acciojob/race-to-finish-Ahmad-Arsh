70d8ed70cb24f2ed061df422150a20bc4692f7c6
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-27 12:54:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 window.promises = [];
+const promises = [];
 
+for (let i = 0; i < 5; i++) {
+  const promise = new Promise((resolve) => {
+    const delay = Math.floor(Math.random() * 5) + 1;
+    setTimeout(() => resolve(delay), delay * 1000);
+  });
+  promises.push(promise);
+}
+
+Promise.any(promises)
+  .then((result) => {
+    document.getElementById("output").innerHTML = result;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 // Do not change the code above this
 // add your promises to the array `promises`
